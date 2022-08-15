2d2238a52896eb912605dc19b64503d0ca4affaf
Stanley <stanleykywu@gmail.com>
2022-08-15 13:34:40 -0400

    Link to blog

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeaderSocials from './HeaderSocials'
 import CTA from './CTA'
-import { Container } from '@mui/material'
+import { Button, Container } from '@mui/material'
 
 const Header = () => {
   return (
@@ -11,6 +11,20 @@ const Header = () => {
         <h1 style={{ marginTop: 0, marginBottom: 10, fontSize: 40 }}>Stanley Wu</h1>
         <h5 className="text__light">Machine Learning Scientist/Engineer</h5>
         <CTA />
+        <Button 
+          size='large'
+          variant="outlined" 
+          href={'./blogs'}
+          sx={{
+            marginTop: 5,
+            marginBottom: 1,
+            color: '#4db5ff',
+            background: 'linear-gradient(to right bottom, #430089, #003e75)'
+          }}
+          className={"white__hover"}
+        >
+          Check Out My Blog!
+        </Button>
         <HeaderSocials />
       </div>  
     </Container>
