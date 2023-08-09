07344283efafee81cf6cc01615a66cdc7685a696
Stanley <stanleykywu@gmail.com>
2023-08-08 22:13:15 -0700

    major revamp

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import CTA from './cta/CTA'
-import { Container, Grid, Avatar } from '@mui/material'
-import Box from '@mui/material/Box';
+import { Container, Grid, Avatar, ListItem, List } from '@mui/material'
 import ME from '../../../assets/me.png'
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-import { BsInstagram } from 'react-icons/bs';
-import {FiLinkedin} from 'react-icons/fi'
-import {VscGithub} from 'react-icons/vsc'
-import IconButton from '@mui/material/IconButton'
-import { AiFillMail } from 'react-icons/ai';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faGoodreads, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLocationDot, faEnvelope, faGraduationCap, faFilePdf } from '@fortawesome/free-solid-svg-icons'
+import RESUME from '../../../assets/resume.pdf'
 
 const Header = () => {
   const theme = useTheme();
@@ -20,43 +18,94 @@ const Header = () => {
       <Grid container display={(isMobile) ? 'block' : 'flex'} direction={(isMobile) ? 'column' : "row"} justifyContent="center" paddingTop={12}> 
         <Grid item md={4} paddingTop={2}>
           <Avatar alt='Stanley Wu' src={ME} variant='circular' sx={{ width: '300px', height: '300px' }} style={{background: 'linear-gradient(to right bottom, #96C2DB, transparent)'}}/>
-          <Box  
-            sx={{
-              flexDirection: 'row',
-              paddingTop: 0.5,
-              paddingLeft: 1,
-            }}
-          >
-            <IconButton variant='contained' href='https://linkedin.com/in/stanleykywu' target="_blank">
-              <FiLinkedin style={{color: '#adc3ef'}} className={"white__hover"}/>
-            </IconButton>
-            <IconButton variant='contained' href='https://github.com/stanleykywu' target="_blank">
-              <VscGithub style={{color: '#adc3ef'}} className={"white__hover"}/>
-            </IconButton>
-            <IconButton variant='contained' href='https://www.instagram.com/stanleykywu/' target="_blank">
-              <BsInstagram style={{color: '#adc3ef'}} className={"white__hover"}/>
-            </IconButton>
-            <IconButton variant='contained' sx={{ paddingTop: 2 }}>
-              <a href='mailto: stanleykywu@gmail.com'>
-                <AiFillMail style={{color: '#adc3ef'}} className={"white__hover"}/>
-              </a>
-            </IconButton>
-          </Box>
-          <CTA />
+          <CTA/>
         </Grid>
         <Grid md={1}/>
         <Grid item md={7} textAlign="left">
           <h1 style={{ marginTop: 0, marginBottom: 10, fontSize: 40 }}>Stanley Wu</h1>
+          
+          <Grid container spacing={0} alignItems="left" justifyContent="left">
+            <Grid item xs={4}> 
+              <List sx={{ paddingLeft: "0px", marginLeft: "0px" }}>
+                <ListItem>
+                  <i aria-hidden="true" style={{ marginRight: "5px"}}>
+                    <FontAwesomeIcon icon={faLocationDot} style={{ color: "#adc3ef"}}></FontAwesomeIcon>
+                  </i>
+                  Chicago, IL
+                </ListItem>
+                <ListItem>
+                  <a href='mailto: stanleykywu@gmail.com' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faEnvelope} style={{ color: "#adc3ef"}}></FontAwesomeIcon>
+                    </i>
+                    Email
+                  </a>
+                </ListItem>
+                <ListItem>
+                  <a href={RESUME} target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faFilePdf} style={{ color: "#adc3ef"}}></FontAwesomeIcon>
+                    </i>
+                    CV
+                  </a>
+                </ListItem>
+                <ListItem>
+                  <a href='https://scholar.google.com/citations?user=wkis3pgAAAAJ&hl=en' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faGraduationCap} style={{ color: "#adc3ef"}}></FontAwesomeIcon>
+                    </i>
+                    Google Scholar
+                  </a>
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid item xs={4}>
+              <List>
+                <ListItem>
+                  <a href='https://linkedin.com/in/stanleykywu' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faLinkedin} style={{ color: "#0077B5"}}></FontAwesomeIcon>
+                    </i>
+                    LinkedIn
+                  </a>
+                </ListItem>
+                <ListItem>
+                  <a href='https://github.com/stanleykywu' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faGithub} style={{ color: "black"}}></FontAwesomeIcon>
+                    </i>
+                    GitHub
+                  </a>
+                </ListItem>
+                <ListItem>
+                  <a href='https://twitter.com/the_stanleywu' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faXTwitter} style={{ color: "black"}}></FontAwesomeIcon>
+                    </i>
+                    Twitter
+                  </a>
+                </ListItem>
+                <ListItem>
+                  <a href='https://goodreads.com/stanleywu' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>
+                    <i style={{ marginRight: "5px"}}>
+                      <FontAwesomeIcon icon={faGoodreads} style={{ color: "#553b08"}}></FontAwesomeIcon>
+                    </i>
+                    goodreads
+                  </a>
+                </ListItem>
+              </List>
+            </Grid>
+          </Grid> 
           <p align="left" style={{  }}>
-            I am a Ph.D. student at the University of Chicago <a href='https://sandlab.cs.uchicago.edu/' target="_blank" rel="noreferrer">SAND Lab</a>, co-advised by <a href='https://people.cs.uchicago.edu/~ravenben/' target="_blank" rel="noreferrer">Ben Zhao</a> and <a href='https://people.cs.uchicago.edu/~htzheng/' target="_blank" rel="noreferrer">Heather Zheng</a>. 
+            I am a Ph.D. student at the University of Chicago <a href='https://sandlab.cs.uchicago.edu/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>SAND Lab</a>, co-advised by <a href='https://people.cs.uchicago.edu/~ravenben/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Ben Zhao</a> and <a href='https://people.cs.uchicago.edu/~htzheng/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Heather Zheng</a>. 
           </p>
           <p align="left">
             My primary academic interests lie in trustworthy machine learning, and I am currently focused on researching solutions to those affected by unethical AI/ML systems.
           </p>
           <p>
-            I received my bachelors in computer science from Northeastern University, during which I was very fortunate to work with <a href='https://www.ccs.neu.edu/home/alina/' target="_blank" rel="noreferrer">Alina Oprea</a>, and <a href='https://jonathan-ullman.github.io/' target="_blank" rel="noreferrer">Jonathan Ullman</a>. I have held several industry positions in the past, mainly in software engineering and data science. Recently, I was a data scientist at <a href='https://www.klaviyo.com/' target="_blank" rel="noreferrer">Klaviyo</a> and <a href='https://www.proofpoint.com/us' target="_blank" rel="noreferrer">Proofpoint</a>.
+            I received my bachelors in computer science from Northeastern University, during which I was very fortunate to work with <a href='https://www.ccs.neu.edu/home/alina/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Alina Oprea</a>, and <a href='https://jonathan-ullman.github.io/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Jonathan Ullman</a>. I have held several industry positions in the past, mainly in software engineering and data science. Recently, I was a data scientist at <a href='https://www.klaviyo.com/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Klaviyo</a> and <a href='https://www.proofpoint.com/us' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Proofpoint</a>.
           </p>  
-          <p><b>Email:</b> stanleywu AT cs.uchicago.edu</p>
+          <p><b>Email:</b> stanleywu AT cs DOT uchicago DOT edu</p>
         </Grid>
       </Grid>
     </Container>
--- src/pages/home/header/cta/CTA.jsx
+++ src/pages/home/header/cta/CTA.jsx
@@ -1,47 +1,15 @@
-import { Button, Grid } from '@mui/material'
+import { Box } from '@mui/material'
 import React from 'react'
-import RESUME from '../../../../assets/resume.pdf'
 
 const CTA = () => {
   return (
-    <Grid container alignItems="center" paddingTop={1} paddingBottom={2} columns={{ xs: 13, sm: 8, md: 15 }}>
-      <Grid xs={1} md={5}/>
-      <Grid xs={5} md={2}>
-        <Button 
-          variant="contained" 
-          href={RESUME}
-          target="_blank"
-          disableElevation
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            backgroundColor: '#adc3ef',
-            color: '#002647',
-          }}
-          className={"slight__hover"}
-        >
-          CV
-        </Button>
-      </Grid>
-      <Grid xs={1} md={1}/>
-      <Grid xs={5} md={2}>
-        <Button 
-          variant="contained" 
-          href={'./blogs'}
-          disableElevation
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            backgroundColor: '#adc3ef',
-            color: '#002647',
-          }}
-          className={"slight__hover"}
-        >
-          Blog
-        </Button>
-      </Grid>
-      <Grid xs={1} md={5}/>
-    </Grid>
+    <Box bgcolor="#E5EDF1" sx={{ borderRadius: '16px', margin: '35px', padding: '5px'}}>
+      <h4><b>FRESH OFF THE PRESS</b></h4>
+      <i>"Calling all artists, we recently deployed a web version for Glaze. Please check it out <a href='https://glaze.cs.uchicago.edu/webglaze.html' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>here</a>!"</i>
+      <br></br>
+      <i>~ Stanley</i>
+      
+    </Box>
   )
 }
 
