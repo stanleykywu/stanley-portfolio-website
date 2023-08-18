6d78adab1ba7e30644328f6d47d6b5fdf4ca70a4
Stanley <stanleykywu@gmail.com>
2023-08-17 20:33:00 -0400

    better link name

--- src/pages/home/header/cta/CTA.jsx
+++ src/pages/home/header/cta/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <Box bgcolor="#E5EDF1" sx={{ borderRadius: '16px', margin: '35px', padding: '5px'}}>
       <h4><b>FRESH OFF THE PRESS</b></h4>
-      <i>"Calling all artists, we recently deployed a web version for Glaze. Please check it out <a href='https://glaze.cs.uchicago.edu/webglaze.html' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>here</a>!"</i>
+      <i>"Calling all artists, we recently deployed a web version for Glaze. Please check it out on our <a href='https://glaze.cs.uchicago.edu/webglaze.html' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Glaze homepage</a>!"</i>
       <br></br>
       <i>~ Stanley</i>
       
